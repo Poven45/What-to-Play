f89bcac343e053577a80ea6a47f6c508b582a2f9
Palwinder Sangha <sanghap@mail.wou.edu>
2024-07-29 10:27:37 -0700

    Platform Icons implemented

---
+++ src/components/PlatformIcons.tsx
@@ -0,0 +1,34 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid, FaGlobe, FaMobileAlt } from "react-icons/fa";
+import { BsNintendoSwitch } from "react-icons/bs";
+
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Props {
+    platforms: Platform[],
+}
+
+const iconMap: {[key: string]: IconType} = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: BsNintendoSwitch,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: FaMobileAlt ,
+    web: FaGlobe,
+    android: FaAndroid
+}
+
+const PlatformIcons = ({platforms}: Props) => {
+    return (
+        <HStack marginY={1}>
+            {platforms.map((platform) => (
+                <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+            ))}
+        </HStack> 
+    )
+}
+
+export default PlatformIcons;
