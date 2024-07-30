8e9b022aca2de91822971b7d5b2ab835af285a0a
Palwinder Sangha <sanghap@mail.wou.edu>
2024-07-30 08:31:11 -0700

    added genres with background image

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
 	const { data } = useGenres();
-	return <ul>
-        {data.map((genre) => <li key={genre.id}>{genre.name}</li>)}
-    </ul>;
+	return (
+		<List>
+			{data.map((genre) => (
+				<ListItem key={genre.id} paddingY={1}>
+					<HStack>
+						<Image
+							boxSize={"40px"}
+							borderRadius={10}
+							src={getCroppedImageUrl(genre.image_background)} alt={"Genre Image of " + genre.name}
+						/>
+                        <Text fontSize="lg">{genre.name}</Text>
+					</HStack>
+				</ListItem>
+			))}
+		</List>
+	);
 };
 
 export default GenreList;
