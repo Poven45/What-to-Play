43b19d2e41d5b5d6d1e9335cd4c4e471f54a40a9
ps <povensangha@ymail.com>
2024-07-31 10:09:51 -0700

    Part 1 of 2 done, build task is up

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api/",
     params: {
+        // YES I KNOW YOU CAN SEE THIS ITS FINE LOL
         key: "1d03e2d1bfb94fa08c9ff379e52b1050"
     }
 })
