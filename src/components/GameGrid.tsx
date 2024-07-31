1d831a5a2079e14172821393aef6e9f76b8a8f64
Palwinder Sangha <sanghap@mail.wou.edu>
2024-07-31 08:51:37 -0700

    moved error in gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,27 +14,28 @@ const GameGrid = ({ gameQuery }: Props) => {
 	const { data, error, isLoading } = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
+	if (error) {
+		return <Text>{error}</Text>;
+	}
+
 	return (
-		<>
-			{error && <Text>{error}</Text>}
-			<SimpleGrid
-				columns={{ sm: 2, md: 3, lg: 4, xl: 5 }}
-				padding={5}
-				spacing={4}
-			>
-				{isLoading &&
-					skeletons.map((skeleton) => (
-						<GameContainer key={skeleton}>
-							<GameSkeleton />
-						</GameContainer>
-					))}
-				{data.map((game) => (
-					<GameContainer key={game.id}>
-						<GameCard game={game} />
+		<SimpleGrid
+			columns={{ sm: 2, md: 3, lg: 4, xl: 5 }}
+			padding={5}
+			spacing={4}
+		>
+			{isLoading &&
+				skeletons.map((skeleton) => (
+					<GameContainer key={skeleton}>
+						<GameSkeleton />
 					</GameContainer>
 				))}
-			</SimpleGrid>
-		</>
+			{data.map((game) => (
+				<GameContainer key={game.id}>
+					<GameCard game={game} />
+				</GameContainer>
+			))}
+		</SimpleGrid>
 	);
 };
 
