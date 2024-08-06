8d00ec1313102625a4ea17089731fd44e9be3efc
ps <povensangha@ymail.com>
2024-08-06 15:07:37 -0700

    refactored stale time with ms

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { Response } from "../services/api-client";
 import { Platform } from "./usePlatform";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -31,6 +32,7 @@ const useGames = (gameQuery: GameQuery) =>
 		getNextPageParam: (lastPage, allPages) => {
 			return lastPage.next ? allPages.length + 1 : undefined;
 		},
+		staleTime: ms("24h")
 	});
 
 export default useGames;
