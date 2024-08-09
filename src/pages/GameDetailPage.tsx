40a0b27888f368849e260bba123d364412807d46
ps <povensangha@ymail.com>
2024-08-09 11:19:35 -0700

    added videos to game cards

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,18 +1,14 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import useGame from "../entities/useGame";
 import {
 	Box,
 	Heading,
-	SimpleGrid,
 	Spinner,
-	Text,
 	useColorMode,
 } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes"
+import GameTrailer from "../components/GameTrailer"
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -34,6 +30,7 @@ const GameDetailPage = () => {
 			<Heading>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
 			<GameAttributes game={game} />
+            <GameTrailer gameId={game.id} />
 		</Box>
 	);
 };
